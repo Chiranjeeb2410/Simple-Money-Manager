b2d06d74ae3411902c973766008b61362adb7261
Chiranjeeb Mahanta <chiranjeebm2410@gmail.com>
2019-04-08 11:24:54 +0530

    display transaction class

--- app/src/main/java/com/example/simplemoneymanager/TransactionActivity.java
+++ app/src/main/java/com/example/simplemoneymanager/TransactionActivity.java
@@ -1,4 +1,14 @@
 package com.example.simplemoneymanager;
 
-public class TransactionActivity {
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TransactionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_transaction);
+        getSupportActionBar().setTitle("Transaction Details");
+    }
 }
--- app/src/main/java/com/example/simplemoneymanager/UserActivity.java
+++ app/src/main/java/com/example/simplemoneymanager/UserActivity.java
@@ -96,6 +96,7 @@ public class UserActivity extends AppCompatActivity {
                                     }
                                 });
                                 builder.create().show();
+                                break;
                         }
 
                     }
