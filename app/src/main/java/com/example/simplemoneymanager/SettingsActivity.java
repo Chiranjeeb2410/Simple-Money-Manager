7a6b650d245e21f03decf024c2ec9f4f9300e92f
Chiranjeeb Mahanta <chiranjeebm2410@gmail.com>
2019-05-02 15:05:29 +0530

    ui changes

---
+++ app/src/main/java/com/example/simplemoneymanager/SettingsActivity.java
@@ -0,0 +1,15 @@
+package com.example.simplemoneymanager;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.EditText;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+
+    }
+}
